071769445d9f99ed1b5b3fd24acac60b8060309e
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-12-04 09:02:12 +0530

    feat: Implementing tmdb Api

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import { instance } from "../config/axios";
+import { requests } from "../config/Requests";
+
 export const Banner = () => {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const req = await instance.get(requests.fetchNetflixOriginals);
+      setMovie(
+        req.data.results[
+          Math.floor(Math.random() * req.data.results.length - 1)
+        ]
+      );
+    };
+    fetchData();
+  }, []);
+
   const truncate = (string, n) => {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   };
@@ -6,18 +23,20 @@ export const Banner = () => {
     <header
       className="banner"
       style={{
-        background: `url("https://i.imgur.com/e1hLQ2m.png")`,
         backgroundSize: "cover",
+        background: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner_contents">
-        <h1 className="banner_title">MovieName</h1>
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_desc">{truncate(`This is a test desc`, 150)}</h1>
+        <h1 className="banner_desc">{truncate(movie?.overview, 150)}</h1>
       </div>
       <div className="banner_fadeBottom" />
     </header>
