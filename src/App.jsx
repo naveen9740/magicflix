d7085aa9413bfdb93611635d72742601d741ddf1
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-12-05 14:42:37 +0530

    Login and Signup functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import "./App.css";
 import { HomeScreen } from "./pages/HomeScreen/HomeScreen";
 import { Route, Routes, Link } from "react-router-dom";
 import { Login } from "./pages/Login/Login";
+import { useEffect } from "react";
+import { auth } from "./firebase";
 
 const App = () => {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged In
+      } else {
+        // Logged Out
+      }
+    });
+    return unsubscribe;
+  }, []);
   const user = false;
   return (
     <div className="app">
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,16 +1,49 @@
+import { useRef } from "react";
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "../../firebase";
 import "./SignIn.css";
 
 export const SignIn = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((userAuth) => console.log(userAuth))
+      .catch((err) => console.log(err.message));
+  };
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((userAuth) => console.log(userAuth))
+      .catch((err) => alert(err.message));
+  };
   return (
     <div className="signIn">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
         <h4>
           <span className="signIn_gray">New to Magicflix? </span>
-          <span className="signIn_link">Sign Up Now</span>
+          <span className="signIn_link" onClick={register}>
+            Sign Up Now
+          </span>
         </h4>
       </form>
     </div>
